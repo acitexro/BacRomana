ee772e2785ec480352ce574bbbb30a6e17aa5414
Alexandru 'acitex' Epure <alexcristian267@gmail.com>
2019-12-27 17:29:20 +0200

    Initial Commit

---
+++ js/subobjects.js
@@ -0,0 +1,32 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function myFunction2() {
+    var x = document.getElementById("myDIVopt2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function myFunction3() {
+    var x = document.getElementById("myDIVopt3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  $(".myBox").click(function() {
+    window.location = $(this).find("a").attr("href"); 
+    return false;
+  });
+  
